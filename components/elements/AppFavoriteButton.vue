9354795109b4f223ede0d4d81c596064fb6bfdeb
dedeard <dedeariansya1@gmail.com>
2019-11-26 22:18:00 +0800

    favorite button

---
+++ components/elements/AppFavoriteButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="favorite-button">
+    <heart-icon />
+  </button>
+</template>
+<script>
+import HeartIcon from '@/components/icons/Heart'
+export default {
+  components: {
+    HeartIcon
+  }
+}
+</script>
