226a24184ac451e9b5998506fa3e3370a1e145b8
dedeard <dedeariansya1@gmail.com>
2019-11-26 20:40:14 +0800

    home category

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <home-banner></home-banner>
+    <home-category></home-category>
   </div>
 </template>
 <script>
 import HomeBanner from '~/components/collections/home/HomeBanner'
+import HomeCategory from '~/components/collections/home/HomeCategory'
 export default {
   layout: 'app',
   components: {
-    HomeBanner
+    HomeBanner,
+    HomeCategory
   }
 }
 </script>
