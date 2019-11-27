c48c5a683ca7b35a8eb5bb0a8e8296b432205241
dedeard <dedeariansya1@gmail.com>
2019-11-27 21:11:54 +0800

    product entry nav

---
+++ components/pages/product/EntryNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid entry-nav py-3">
+    <div class="entry-nav-wrapper">
+      <div class="left">
+        <div class="entry-nav-title-key">Hasil pencarian</div>
+        <h1 class="entry-nav-title-value">
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit"
+        </h1>
+      </div>
+      <div class="right">
+        <label class="entry-nav-sort-option-label">Urutkan</label>
+        <select class="entry-nav-sort-option">
+          <option selected>Terbaru</option>
+          <option>Termurah</option>
+          <option>Termahal</option>
+          <option>Terlaris</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/product/index.vue
+++ pages/product/index.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="product-page"></div>
+  <div class="product-page">
+    <product-entry-nav></product-entry-nav>
+    <div class="divider"></div>
+  </div>
 </template>
 <script>
+import ProductEntryNav from '~/components/pages/product/EntryNav'
 export default {
-  layout: 'app'
+  layout: 'app',
+  components: {
+    ProductEntryNav
+  }
 }
 </script>
