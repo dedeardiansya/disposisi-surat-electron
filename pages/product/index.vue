6d1008fbe1e84f83b09770e1f0be48db1c0234c5
dedeard <dedeariansya1@gmail.com>
2019-11-28 02:17:07 +0800

    product results

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -2,14 +2,17 @@
   <div class="product-page">
     <product-entry-nav></product-entry-nav>
     <div class="divider"></div>
+    <product-results></product-results>
   </div>
 </template>
 <script>
 import ProductEntryNav from '~/components/pages/product/EntryNav'
+import ProductResults from '~/components/pages/product/Results'
 export default {
   layout: 'app',
   components: {
-    ProductEntryNav
+    ProductEntryNav,
+    ProductResults
   }
 }
 </script>
